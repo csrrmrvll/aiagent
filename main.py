c4d3922178cccb30c6e31102a849d1db22092aa5
César Romero Avello <cesar.romero.avello@ing.com>
2026-06-21 13:14:58 +0200

    feat: response metadata, tokens, included

--- main.py
+++ main.py
@@ -4,17 +4,23 @@ from google import genai
 
 
 def main():
-    print("Hello from aiagent!")
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
     if api_key is None:
         raise RuntimeError("api key not found")
     client = genai.Client(api_key=api_key)
+    user_prompt = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+    print(f"User prompt: {user_prompt}")
     response = client.models.generate_content(
         model="gemini-2.5-flash",
-        contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.",
+        contents=user_prompt,
     )
-    print(response.text)
+    usage_metadata = response.usage_metadata
+    if usage_metadata is None:
+        raise RuntimeError("usage metadata is missing, failed API request")
+    print(f"Prompt tokens: {usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {usage_metadata.candidates_token_count}")
+    print(f"Response: {response.text}")
 
 
 if __name__ == "__main__":
