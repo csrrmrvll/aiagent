49e6dd2ae275960bb9a17d870702b5e2121d79dd
César Romero Avello <cesar.romero.avello@ing.com>
2026-06-21 13:20:29 +0200

    feat: added user prompt as an input argument

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
 import os
+import argparse
 from dotenv import load_dotenv
 from google import genai
 
 
 def main():
+    parser = argparse.ArgumentParser(description="aiagent")
+    parser.add_argument("user_prompt", type=str, help="User prompt")
+    args = parser.parse_args()
+    user_prompt = args.user_prompt
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
     if api_key is None:
         raise RuntimeError("api key not found")
     client = genai.Client(api_key=api_key)
-    user_prompt = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
     print(f"User prompt: {user_prompt}")
     response = client.models.generate_content(
         model="gemini-2.5-flash",
